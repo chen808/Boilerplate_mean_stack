cacc3531ea66d94d3bcbc06ea509a1b881d199ea
David Chen <chentiki808@gmail.com>
2016-04-19 19:54:46 -0700

    initial commit

---
+++ server/controllers/tweets.js
@@ -0,0 +1,22 @@
+// TWEETS CONTROLLER
+
+var mongoose = require('mongoose');
+var Tweet = mongoose.model('Tweet');
+
+
+module.exports = (function(){
+
+	return {
+
+		// methods goes here
+
+
+
+	}
+
+
+
+
+
+
+})
