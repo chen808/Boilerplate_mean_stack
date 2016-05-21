fdab65b8b9b6916b3dbced3397756daa08579895
David Chen <chentiki808@gmail.com>
2016-05-20 22:58:45 -0700

    added port listen

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var path = require('path');
 // instantiate the express app
 var app = express();
 
+// the following is for HEROKU setup
+app.set('port', (process.env.PORT || 5000))
+
+
 // require body-parser
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -23,12 +27,14 @@ var route_setter = require('./server/config/routes.js');
 route_setter(app);
 
 
+app.get('/', function(request, response){
+	response.render('')
+})
 
 
 
-
-// set up static file server
-app.use(express.static(path.join(__dirname,'./client')));
-app.listen(8000, function(){
-	console.log('Batman is listening on port 8000');
-});
+// // set up static file server
+// app.use(express.static(path.join(__dirname,'./client')));
+// app.listen(8000, function(){
+// 	console.log('Batman is listening on port 8000');
+// });
